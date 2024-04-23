7dd34ecd168dd3a2eeaad8b7abc1152c666bacbb
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-23 20:20:06 +0700

    ✅refactor

---
+++ src/components/Input.tsx
@@ -0,0 +1,31 @@
+import React, { InputHTMLAttributes } from 'react';
+
+type InputPropsType = {
+   setInputTitle: (title: string) => void;
+   onKeyUp: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+   className: string;
+   inputTitle: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export const Input = (props: InputPropsType) => {
+   const inputOnKeyUpHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      props.onKeyUp(event);
+   };
+   const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (event.currentTarget.value.length < 15) {
+         let inputTitle = event.currentTarget.value;
+         props.setInputTitle(inputTitle);
+      } else {
+         alert('Task title must be less than 15 characters');
+      }
+   };
+
+   return (
+      <input
+         value={props.inputTitle}
+         className={props.className}
+         onKeyUp={inputOnKeyUpHandler}
+         onChange={inputChangeHandler}
+      ></input>
+   );
+};
