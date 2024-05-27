f3a07f2759637db6aff4f0cf05c28e94a8055ba2
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-05-27 22:32:12 +0700

    refactor

--- src/model/todolists-reducer/todolists-reducer.ts
+++ src/model/todolists-reducer/todolists-reducer.ts
@@ -6,9 +6,8 @@ let todolistID2 = v1();
 
 export type RemoveTodolistActionType = {
    type: 'REMOVE-TODOLIST';
-   payload: {
-      id: string;
-   };
+
+   id: string;
 };
 
 export type AddTodolistActionType = {
@@ -19,18 +18,16 @@ export type AddTodolistActionType = {
 
 export type ChangeTodolistFilterActionType = {
    type: 'CHANGE-TODOLIST-FILTER';
-   payload: {
-      filter: FilterValuesType;
-      todolistId: string;
-   };
+
+   filter: FilterValuesType;
+   todolistId: string;
 };
 
 export type ChangeTodolistTitleActionType = {
    type: 'CHANGE-TODOLIST-TITLE';
-   payload: {
-      id: string;
-      title: string;
-   };
+
+   id: string;
+   title: string;
 };
 export type ActionsType =
    | RemoveTodolistActionType
@@ -46,18 +43,16 @@ const initialState: TodolistsType[] = [
 export const todolistsReducer = (state: TodolistsType[] = initialState, action: ActionsType): TodolistsType[] => {
    switch (action.type) {
       case 'REMOVE-TODOLIST': {
-         return state.filter((tl) => tl.id !== action.payload.id);
+         return state.filter((tl) => tl.id !== action.id);
       }
       case 'ADD-TODOLIST': {
          return [...state, { id: action.todolistId, title: action.title, filter: 'all' }];
       }
       case 'CHANGE-TODOLIST-FILTER': {
-         return state.map((tl) =>
-            tl.id === action.payload.todolistId ? { ...tl, filter: action.payload.filter } : tl
-         );
+         return state.map((tl) => (tl.id === action.todolistId ? { ...tl, filter: action.filter } : tl));
       }
       case 'CHANGE-TODOLIST-TITLE': {
-         return state.map((tl) => (tl.id === action.payload.id ? { ...tl, title: action.payload.title } : tl));
+         return state.map((tl) => (tl.id === action.id ? { ...tl, title: action.title } : tl));
       }
       default:
          throw new Error("I don't understand this type");
@@ -67,9 +62,8 @@ export const todolistsReducer = (state: TodolistsType[] = initialState, action: 
 export const removeTodolistAC = (todolistId: string): RemoveTodolistActionType => {
    return {
       type: 'REMOVE-TODOLIST',
-      payload: {
-         id: todolistId,
-      },
+
+      id: todolistId,
    } as const;
 };
 export const addTodolistAC = (title: string): AddTodolistActionType => {
@@ -86,19 +80,17 @@ export const changeTodolistFilterAC = (
 ): ChangeTodolistFilterActionType => {
    return {
       type: 'CHANGE-TODOLIST-FILTER',
-      payload: {
-         filter,
-         todolistId,
-      },
+
+      filter,
+      todolistId,
    } as const;
 };
 
 export const changeTodolistTitleAC = (id: string, title: string): ChangeTodolistTitleActionType => {
    return {
       type: 'CHANGE-TODOLIST-TITLE',
-      payload: {
-         id,
-         title,
-      },
+
+      id,
+      title,
    } as const;
 };
