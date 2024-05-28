5f8389271150b0ae8a52f5874701428d7b4de99f
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-05-28 20:07:42 +0700

    refactor

--- src/model/tasks-reducer/tasks-reducer.test.ts
+++ src/model/tasks-reducer/tasks-reducer.test.ts
@@ -3,11 +3,14 @@ import { v1 } from 'uuid';
 import { TasksState } from '../../App';
 import { addTodolistAC } from '../todolists-reducer/todolists-reducer';
 
-test('correct task should be removed', () => {
-   let todolistId1 = v1();
-   let todolistId2 = v1();
+let startState: TasksState;
+let todolistId1: string;
+let todolistId2: string;
 
-   const startState: TasksState = {
+beforeEach(() => {
+   todolistId1 = v1();
+   todolistId2 = v1();
+   startState = {
       [todolistId1]: [
          { id: v1(), title: 'HTML&CSS', isDone: false },
          { id: v1(), title: 'JS', isDone: false },
@@ -21,13 +24,12 @@ test('correct task should be removed', () => {
          { id: v1(), title: 'Meat', isDone: false },
       ],
    };
+});
 
+test('correct task should be removed', () => {
    const theTask = startState[todolistId2][0].id;
-
    const endState = tasksReducer(startState, removeTaskAC(theTask, todolistId2));
-
    expect(endState[todolistId2].length).toBe(1);
-
    expect(endState).toEqual({
       [todolistId1]: startState[todolistId1],
       [todolistId2]: [{ id: startState[todolistId2][1].id, title: 'Meat', isDone: false }],
@@ -35,23 +37,6 @@ test('correct task should be removed', () => {
 });
 
 test('correct task should be added', () => {
-   let todolistId1 = v1();
-   let todolistId2 = v1();
-
-   const startState: TasksState = {
-      [todolistId1]: [
-         { id: v1(), title: 'HTML&CSS', isDone: false },
-         { id: v1(), title: 'JS', isDone: false },
-         { id: v1(), title: 'ReactJS', isDone: false },
-         { id: v1(), title: 'Redux', isDone: false },
-         { id: v1(), title: 'Typescript', isDone: false },
-         { id: v1(), title: 'RTK query', isDone: false },
-      ],
-      [todolistId2]: [
-         { id: v1(), title: 'Fruits', isDone: false },
-         { id: v1(), title: 'Meat', isDone: false },
-      ],
-   };
    const action = addTaskAC('New task', todolistId2);
 
    const endState = tasksReducer(startState, action);
@@ -64,47 +49,12 @@ test('correct task should be added', () => {
 });
 
 test('correct status of task should be changed', () => {
-   let todolistId1 = v1();
-   let todolistId2 = v1();
-
-   const startState: TasksState = {
-      [todolistId1]: [
-         { id: v1(), title: 'HTML&CSS', isDone: false },
-         { id: v1(), title: 'JS', isDone: false },
-         { id: v1(), title: 'ReactJS', isDone: false },
-         { id: v1(), title: 'Redux', isDone: false },
-         { id: v1(), title: 'Typescript', isDone: false },
-         { id: v1(), title: 'RTK query', isDone: false },
-      ],
-      [todolistId2]: [
-         { id: v1(), title: 'Fruits', isDone: false },
-         { id: v1(), title: 'Meat', isDone: false },
-      ],
-   };
-
    const endState = tasksReducer(startState, changeTaskStatusAC(todolistId2, startState[todolistId2][0].id, true));
 
    expect(endState[todolistId2][0].isDone).toBe(true);
 });
 
 test('correct task should change its name', () => {
-   let todolistId1 = v1();
-   let todolistId2 = v1();
-   const startState: TasksState = {
-      [todolistId1]: [
-         { id: v1(), title: 'HTML&CSS', isDone: false },
-         { id: v1(), title: 'JS', isDone: false },
-         { id: v1(), title: 'ReactJS', isDone: false },
-         { id: v1(), title: 'Redux', isDone: false },
-         { id: v1(), title: 'Typescript', isDone: false },
-         { id: v1(), title: 'RTK query', isDone: false },
-      ],
-      [todolistId2]: [
-         { id: v1(), title: 'Fruits', isDone: false },
-         { id: v1(), title: 'Meat', isDone: false },
-      ],
-   };
-
    const endState = tasksReducer(
       startState,
       changeTaskTitleAC(todolistId2, startState[todolistId2][0].id, 'New TaskTitle')
@@ -144,6 +94,3 @@ test('new array should be added when new todolist is added', () => {
    expect(keys.length).toBe(3);
    expect(endState[newKey]).toEqual([]);
 });
-
-
-
