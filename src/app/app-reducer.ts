d3a37ec19d94b6edb00e06daa1588b3d0834630a
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-07-09 21:34:11 +0700

    add app-reducer

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,50 @@
+
+
+export type RequestStatusType = 
+'idle' 
+|
+'loading' 
+| 
+'succeeded' 
+| 
+'failed'
+
+
+const initialState = {
+  status: 'loading' as RequestStatusType,
+  error: null as null | string
+}
+ 
+
+type InitialStateType = typeof initialState
+
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+  switch (action.type) {
+    case 'APP/SET-STATUS':
+      return { 
+		...state,
+		status: action.status 
+		}		
+    case 'APP/SET-ERROR':
+      return { 
+		...state,
+		error: action.error 
+		}		
+    default:
+      return state
+  }
+}
+
+
+export type setLoadingType = ReturnType<typeof setLoadingAC>
+export type setErrorType = ReturnType<typeof setErrorAC>
+export const setLoadingAC = (status: RequestStatusType) =>({ type: 'APP/SET-STATUS', status } as const)
+export const setErrorAC = (error: string | null) =>({ type: 'APP/SET-ERROR', error } as const)
+
+type ActionsType = setLoadingType | setErrorType
+
+ 
+
+
+
