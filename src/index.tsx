dde4eca6aec3a13d382de57fbbd48b45c9f3c036
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-07-21 13:34:07 +0700

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,13 @@
-import './index.css';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import App from './app/App';
-import { Provider } from 'react-redux';
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
-import { Login } from './features/Login/Login';
-import { TodolistsList } from './features/TodolistsList/TodolistsList';
-import { store } from './app/store';
-import { ErrorPage } from './components/ErrorPage/ErrorPage';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./app/App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-const router = createBrowserRouter([
-   {
-      path: '/',
-      element: <App />,
-      errorElement: <Navigate to="/404" />,
-      children: [
-         {
-            index: true,
-            element: <Navigate to="/todolists" />,
-         },
-         {
-            path: '/login',
-            element: <Login />,
-         },
-         {
-            path: '/todolists',
-            element: <TodolistsList />,
-         },
-      ],
-   },
-   {
-      path: '/404',
-      element: <ErrorPage />,
-   },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-   <Provider store={store}>
-      <RouterProvider router={router} />
-   </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
-
-reportWebVitals();
