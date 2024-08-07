6b2dff1ad6e02c3145df12038ed4a9675fd87df3
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-08-07 21:09:08 +0700

    refactor error-utils

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,16 +1,20 @@
 import { Dispatch } from 'redux'
- 
+
 import { setAppError, setAppStatus } from 'app/appSlice'
 import { BaseResponse } from 'common/types/types'
 
-
-
-
-export const handleServerAppError = <T>(dispatch: Dispatch, data: BaseResponse<T>) => {
-  if (data.messages.length) {
-    dispatch(setAppError({ error: data.messages[0] }))
-  } else {
-    dispatch(setAppError({ error: 'Some error occurred' }))
+export const handleServerAppError = <T>(
+  dispatch: Dispatch,
+  data: BaseResponse<T>,
+  isShowGlobalError: boolean = true,
+) => {
+  if (isShowGlobalError) {
+    if (data.messages.length) {
+      dispatch(setAppError({ error: data.messages[0] }))
+    } else {
+      dispatch(setAppError({ error: 'Some error occurred' }))
+    }
   }
+
   dispatch(setAppStatus({ status: 'failed' }))
 }
