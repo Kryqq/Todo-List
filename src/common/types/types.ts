57779e0da97b9c0951321e0a6f9d368fb41703a6
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-08-07 21:00:28 +0700

    add fieldsError in BaseResponse type

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -1,7 +1,13 @@
 export type TestAction<T extends (...args: any) => any> = Omit<ReturnType<T>, 'meta'>
 
+type FiledErrorType = {
+  error: string
+  field: string
+}
+
 export type BaseResponse<D = {}> = {
   resultCode: number
   messages: Array<string>
   data: D
+  fieldsErrors: FiledErrorType[]
 }
