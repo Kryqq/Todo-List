cab2a8bd591fd33560826f3b1ba4e40ef563dc94
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-08-07 20:59:59 +0700

    delted combined reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,18 @@
 import { tasksReducer } from 'features/TodolistsList/tasksSlice'
 import { todolistsReducer } from 'features/TodolistsList/todolistsSlice'
-import { combineReducers } from 'redux'
-import { ThunkAction, ThunkDispatch } from 'redux-thunk'
+
 import { appReducer } from './appSlice'
 
 import { configureStore, UnknownAction } from '@reduxjs/toolkit'
 import { authReducer } from 'features/auth/model/authSlice'
 
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-  auth: authReducer,
+export const store = configureStore({
+  reducer: { tasks: tasksReducer, todolists: todolistsReducer, app: appReducer, auth: authReducer },
 })
 
-export const store = configureStore({ reducer: rootReducer })
+export type AppRootStateType = ReturnType<typeof store.getState>
 
-export type AppRootStateType = ReturnType<typeof rootReducer>
+// export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, UnknownAction>
 
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, UnknownAction>
-
-export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, UnknownAction>
+// export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, UnknownAction>
+export type AppDispatch = typeof store.dispatch
