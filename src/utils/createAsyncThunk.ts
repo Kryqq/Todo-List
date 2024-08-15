432e8604d5eaa397c62e3bcb6ad155b033a27414
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-08-15 23:58:38 +0700

    refactor

--- src/utils/createAsyncThunk.ts
+++ src/utils/createAsyncThunk.ts
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { AppDispatch, AppRootStateType } from 'app/store'
-import { BaseResponse } from 'common/types/types'
+import { RejectActionError } from 'common/types/types'
 
 export const createAppAsyncThunk = createAsyncThunk.withTypes<{
   state: AppRootStateType
   dispatch: AppDispatch
-  rejectValue: null | BaseResponse
+  rejectValue: RejectActionError
 }>()
