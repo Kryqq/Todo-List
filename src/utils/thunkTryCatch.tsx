2b1d27402667b759aa330ae225fa4d4652f1fde0
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-08-15 23:58:51 +0700

    refactor

--- src/utils/thunkTryCatch.tsx
+++ src/utils/thunkTryCatch.tsx
@@ -7,13 +7,12 @@ import { BaseResponse } from 'common/types/types'
 type ThunkAPI = BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | BaseResponse>
 
 
-export const thunkTryCatch = async <T> ({dispatch, rejectWithValue}: ThunkAPI, fn: ()=> Promise<T>,): Promise<T | ReturnType <typeof rejectWithValue>> =>{
+export const thunkTryCatch = async <T> ({ rejectWithValue}: ThunkAPI, fn: ()=> Promise<T>,): Promise<T | ReturnType <typeof rejectWithValue>> =>{
 	try {
-		// dispatch(setAppStatus({status: 'loading'}))
-		return await fn()
+			return await fn()
 	} catch (error) {
 		handleServerNetworkError(dispatch, error)
-		return rejectWithValue(null)
+		return rejectWithValue(error)
 	} finally {
 		// dispatch(setAppStatus({status: 'idle'}))
 	}
